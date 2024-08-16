4c7ffd1c572f31645c47b61d3608937d80bc7789
Jayesh Khairnar <jayeshkhairnar6@gmail.com>
2024-08-16 21:51:10 +0530

    removed unnecessary files

--- Test app for dotnet/DotNetProject/Models/EmailDetails.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DotNet_Backend.Models
-{
-    public class EmailDetails
-    {
-        public string sendTo { get; set; } = string.Empty;
-        public string? path { get; set; } = string.Empty;
-    }
-}
